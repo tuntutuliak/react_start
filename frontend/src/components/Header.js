5e47cf27d6018211b23a6a5f5311c8a167313a02
Ivan Dunupin <tuntutuliak@gmail.com>
2024-01-02 15:17:58 +1000

    initial head component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar} from "react-bootstrap";
 
-const Header = (props) => {
+const Header = ({title}) => {
+
     return(
         <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+            <Navbar.Brand href="/">{title}</Navbar.Brand>
         </Navbar>
     )
 }
