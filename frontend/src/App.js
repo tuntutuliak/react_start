c7b4760863e25b333610c3f4fcf1d35c71c69722
Ivan Dunupin <tuntutuliak@gmail.com>
2024-01-02 20:15:46 +1000

    form implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
+const handleSeachSubmit = (e) => {
+
+  console.log(e)
+
+}
+
 
 const App = ()=> {
   return (
     <div className="App">
       <Header title="Images Gallery"/>
+      <Search handleSubmit={handleSeachSubmit} />
     </div>
   );
 }
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Form, Container, Row, Col, Button } from "react-bootstrap";
+
+const Search = ({ handleSubmit }) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col xs={9}>
+                <Form.Control placeholder="Searching for new image..." />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Search;
